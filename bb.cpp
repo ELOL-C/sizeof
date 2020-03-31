16da24e5372ee64e6e71131e31b3f99907717eda
ZEZE <55626553+EZ-Super@users.noreply.github.com>
2020-03-31 21:08:07 +0800

    Add files via upload

---
+++ bb.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "bool : " << sizeof(bool);
+	cout << "\n char : " << sizeof(char);
+	cout << "\nint : " << sizeof(int);
+	cout << "\nshort : " << sizeof(short int );
+	cout << "\nlong : " << sizeof(long int );
+	cout << "\nfloat : " << sizeof(float);
+	cout << "\ndouble : " << sizeof(double);
+return 0;	
+}
+
+
+
+
+
